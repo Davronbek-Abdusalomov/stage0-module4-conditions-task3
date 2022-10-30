bf32b6290312829acd82aab981360035862c3669
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-30 22:23:39 +0500

    seasons

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.conditions.task3;
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
         if (month==1){
-            System.out.println(31);
+            System.out.print(31);
         }
         else if (month==2){
-            System.out.println(28);
+            System.out.println(30);
         }
         else if (month==3){
             System.out.println(31);
