595ab331b80f081ca0a3513a511a1940804b95a6
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-30 21:27:53 +0500

    seasons

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary<=10000){
+            double taxes = salary*1.5;
+        }
+        else if (10000 < salary && salary <= 20000){
+            double taxes = salary*1.8;
+        }
+        else if (salary>20000){
+            double taxes = salary * 2.0;
+        }
     }
 }
