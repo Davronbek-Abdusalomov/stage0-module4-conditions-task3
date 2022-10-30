844b1f9cd0148f702037abe126ff0d036b6afbc5
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-30 21:49:47 +0500

    seasons

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character>'A' && character<'z'){
+            if (character=='A' || character=='a' || character=='E' || character=='e' || character=='I' || character=='i' || character=='U' || character=='u' || character=='O' || character=='o'){
+                System.out.println("Vowel");
+            }
+            else System.out.println("Consonant");
+        }
+        else System.out.println("wrong alphabet!");
     }
 }
